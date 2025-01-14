a9c58f241ce5a7ba62a1e15e0801dade283d70be
Thu Nguyen <thu.m.nguyen@stonybrook.edu>
2025-01-13 21:07:16 -0500

    darkmode 1st attempt

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-function toggleMenu() {
-    // assign the menu-links class in html file to the menu variable
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon"); // similar
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+function enableDarkmode() {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode','active');
 }
+
+function disableDarkmode() {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode',null);
+}
+
+if (darkmode === "active") {
+    enableDarkmode();
+}
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+})
+
+
+// function toggleMenu() {
+//     // assign the menu-links class in html file to the menu variable
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon"); // similar
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
+
+
